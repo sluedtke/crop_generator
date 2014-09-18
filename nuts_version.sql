94c996078416dad162e573a22fcb7e83a034191c
sluedtke <stefan-luedtke@gmx.de>
2014-09-18 22:34:18 +0200

    Running queries integrated into the R script.

---
+++ nuts_version.sql
@@ -0,0 +1,11 @@
+﻿-- This query returns the newest avalailable nuts version given any nuts code that was valid between 2003 and 2010
+-- Join the unique admin units from the time series ot our reference table
+-- If we find that multiple version fit, we take the most recent (max) one
+-- That will be the reference table for the time series.
+
+SELECT nuts_code, MAX(nuts_version_year)
+from data.nuts_version_years
+WHERE
+ nuts_code= ?
+GROUP BY nuts_code;
+

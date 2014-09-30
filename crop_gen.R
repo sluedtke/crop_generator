2b8b7e2f4e51739d9081b69bb911236a29d90f3a
sluedtke <stefan-luedtke@gmx.de>
2014-09-30 15:56:11 +0200

    The Monte-carlo runs get evaluated.
    * the offset year table was pushed to the DB
    * a function the get the most probable crop given the mc sim  was added.
    * bugfixes at the loops
    * a postgresql function call to update the 'master' result  table

--- crop_gen.R
+++ crop_gen.R
@@ -29,19 +29,22 @@ source("./crop_gen_functions.R")
 ## Get the list of nuts we want to use
 nuts_info_all=nuts()
 
+# And the crops and their minimum offset 
+offset_tab=offset_year()
 
 
-# single_nuts=sample_n(nuts_info_all, 2, replace=F)
+single_nuts=sample_n(nuts_info_all, 2, replace=F)
 # nuts_info=nuts_info_all[nuts_info_all$nuts_code=='DE42', ]
-single_nuts=nuts_info_all[(nuts_info_all$nuts_code=='DE42' | nuts_info_all$nuts_code=='DE41'), ]
+# single_nuts=nuts_info_all[(nuts_info_all$nuts_code=='DE42' | nuts_info_all$nuts_code=='DE41'), ]
 
-mc_runs=1
+mc_runs=10
 
 library(doMPI)
 cl = startMPIcluster()
 registerDoMPI(cl)
-#  
-foreach(i=c(1:nrow(single_nuts)),
+
+
+foreach(i=seq_len(nrow(single_nuts)),
 		.packages=c("RODBCext", "plyr", "dplyr", "dplyrExtras", "reshape2", "foreach",
 					"data.table", "lhs")
 		)%dopar%{
@@ -56,8 +59,6 @@ foreach(i=c(1:nrow(single_nuts)),
 
 		years=unique(ts_data$year)
 
-		offset_tab=data.frame(current_crop_id=c(9, 7, 14), offset_year=c(3, 3, 3))
-
 		# -------------------------- convert to data.tables ---------------------#
 
 		ts_data=as.data.table(ts_data) 
@@ -66,17 +67,22 @@ foreach(i=c(1:nrow(single_nuts)),
 		base_probs=as.data.table(base_probs)
 		setkey(base_probs, current_crop_id, objectid)
 
+
+		offset_tab=as.data.table(lapply(offset_tab, as.numeric))
 		# -----------------------------------------------------------------------#
 
-		temp=foreach(run=mc_runs, .combine="rbind")%do%{
+		mc_temp=foreach(run=seq_len(mc_runs), .combine="rbind")%do%{
 
 				temp=crop_distribution(nuts_base_probs=base_probs, nuts_base_ts=ts_data,
 								  years=years, soil_para=1, follow_up_crop_para=1)
-				temp$mc_run=run
+				temp$mc_run=factor(run)
 				temp=temp
 		}
+		
+		# summarize the mc runs
+		mc_temp=summarize_mc(mc_temp)
 
-		upload_data(nuts_info, data=temp)
+		upload_data(nuts_info, data=mc_temp, prefix="stat")
 }
 
 closeCluster(cl)

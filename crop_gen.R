e6f3d898d0ded7be981d707dbb449dae4dfc3fab
sluedtke <stefan-luedtke@gmx.de>
2014-10-13 10:56:23 +0200

    Hope that helps to debug the problem with multiple hosts.

--- crop_gen.R
+++ crop_gen.R
@@ -1,4 +1,5 @@
-#!/usr/local/R/R-3.1.0/bin/Rscript
+#!/usr/bin/Rscript
+### !/usr/local/R/R-3.1.0/bin/Rscript
 ################################################################################################
 
 #       Filename: crop_gen.R
@@ -38,13 +39,12 @@ max_tab=max_year() %>%
 		rename(., c("offset_year" = "max_seq_year"))
 
 
-mc_runs=100
-
 library(doMPI)
 cl = startMPIcluster()
 registerDoMPI(cl)
 
-foreach(i=seq_len(nrow(nuts_info_all)),
+temp=foreach(i=seq_len(nrow(nuts_info_all)),
+	   	.combine="rbind",
 		.packages=c("RODBCext", "plyr", "dplyr", "dplyrExtras", "reshape2", "foreach",
 					"data.table"), 
 		.errorhandling='pass'
@@ -53,54 +53,11 @@ foreach(i=seq_len(nrow(nuts_info_all)),
 		#subset the dataframe
 		nuts_info=nuts_info_all[i,]
 
-		# -------------------------- Data import --------------------------------#
+		host=system('hostname', intern=T)
 
-		ts_data=nuts_ts(nuts_id=nuts_info$nuts_code) %>%
-				mutate(., value=as.numeric(as.character(value)))
-
-		base_probs=nuts_probs(nuts_info) %>%
-				mutate(., current_soil_prob=as.numeric(as.character(current_soil_prob))) %>%
-				mutate(., follow_up_soil_prob=as.numeric(as.character(follow_up_soil_prob)))
-
-		years=unique(ts_data$year)
-
-		# -------------------------- convert to data.tables ---------------------#
-
-		ts_data=as.data.table(ts_data) 
-		
-		setkey(ts_data, crop_id, year)
-
-		base_probs=as.data.table(base_probs)
-		setkey(base_probs, current_crop_id, objectid)
-
-
-		offset_tab=as.data.table(lapply(offset_tab, as.numeric))
-		max_tab=as.data.table(lapply(max_tab, as.numeric))
-		# -----------------------------------------------------------------------#
-
-		# Some nuts unit are not covered by the EU-refgrid, the have 0 rows in the base_probs tab
-		# we check for that and break the loop if that is the case
-		if(nrow(base_probs)==0){
-				cat("Nuts unit not covered by the EU-RefGrid \n")
-		}else{
-				mc_temp=foreach(run=seq_len(mc_runs), .combine="rbind")%do%{
-
-						temp=crop_distribution(nuts_base_probs=base_probs, nuts_base_ts=ts_data,
-										  years=years, soil_para=1, follow_up_crop_para=1)
-						temp$mc_run=factor(run)
-						temp=temp
-				}
-				
-				# summarize the mc runs
-				mc_temp=summarize_mc(mc_temp)
-
-				# joining the unique identifier from the postgres table
-				mc_temp$oid_nuts=nuts_info$id
-				
-				# upload the features to the DB
-				upload_data(nuts_info, data=mc_temp, prefix="stat")
-
-		}
+		temp=data.frame(res=paste("I am", i,  "on node",
+								  host, "working on", nuts_info$nuts_code))
 }
-
 closeCluster(cl)
+
+print(temp)

58a5320727e5d9fffaf0642cf659c6db99d79450
sluedtke <stefan-luedtke@gmx.de>
2014-10-15 17:21:00 +0200

    Running local version

--- crop_gen.R
+++ crop_gen.R
@@ -15,11 +15,21 @@
 rm(list=ls())
 
 ################################################################################################
+# Initialize MPI
+library("Rmpi")
+
+# Notice we just say "give us all the slaves you've got."
+
+if (mpi.comm.size() < 2) {
+		print("More slave processes are required.")
+		mpi.quit()
+}else{}
+
+################################################################################################
 library(plyr)
 library(dplyr)
 library(dplyrExtras)
 library(reshape2)
-library(foreach)
 library(data.table)
 library(RODBCext)
 
@@ -27,37 +37,129 @@ library(RODBCext)
 source("./crop_gen_functions.R")
 ############################################################################
 
+mpiwrapper = function() {
+		# Note the use of the tag for sent messages: 
+		#     1=ready_for_task, 2=done_task, 3=exiting 
+		# Note the use of the tag for received messages: 
+		#     1=task, 2=done_tasks 
+		junk = 0 
+		done = 0 
+		while (done != 1) {
+				# Signal being ready to receive a new task 
+				mpi.send.Robj(junk, 0, 1) 
+
+				# Receive a task 
+				task = mpi.recv.Robj(mpi.any.source(),mpi.any.tag()) 
+
+				# 2 dim int vector. The first integer is the source and the second is
+				# the tag.
+				task_info = mpi.get.sourcetag() 
+				tag = task_info[2] 
+
+				# 1= we got something to do ..
+				if (tag == 1) {
+						# get the data from the r-object ..
+						id = task$id
+
+						# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+						# the actual job ... 
+						# nuts_info=as.data.frame(nuts_info_all[id,])
+						nuts_info=as.data.frame(t(nuts_info_all[id,]))
+
+						temp=data.frame(res=paste("I am ", id,  " on node " ,
+												  mpi.get.processor.name(),
+												  " working on ",
+												  nuts_info$nuts_code))
+
+						# Send a results message back to the master
+						results = list(result=temp, id=id)
+						mpi.send.Robj(results, 0, 2)
+						# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+				}
+				## We got nothing to do, all tasks done 
+				else if (tag == 2) {
+						# set done to 1, so the loop is not repeated
+						done = 1
+				}
+				# We'll just ignore any unknown messages
+		}
+		mpi.send.Robj(junk, 0, 3)
+}
+
 ## Get the list of nuts we want to use
 nuts_info_all=nuts()
 
-# And the crops and their minimum offset 
-offset_tab=offset_year()
-
-# And the crops and their maximum continuous occurrence 
-max_tab=max_year() %>%
-		rename(., c("current_crop_id" = "follow_up_crop_id")) %>%
-		rename(., c("offset_year" = "max_seq_year"))
+nuts_info_all=as.matrix(nuts_info_all)
+mpi.bcast.Robj2slave(nuts_info_all)
 
 
-library(doMPI)
-cl = startMPIcluster()
-registerDoMPI(cl)
-
-temp=foreach(i=seq_len(nrow(nuts_info_all)),
-	   	.combine="rbind",
-		.packages=c("RODBCext", "plyr", "dplyr", "dplyrExtras", "reshape2", "foreach",
-					"data.table"), 
-		.errorhandling='pass'
-		)%dopar%{
-		
-		#subset the dataframe
-		nuts_info=nuts_info_all[i,]
-
-		host=system('hostname', intern=T)
-
-		temp=data.frame(res=paste("I am", i,  "on node",
-								  host, "working on", nuts_info$nuts_code))
+# Create task list
+tasks = vector('list')
+for (i in 1:nrow(nuts_info_all)) {
+# for (i in 1:length(nuts_info_all)) {
+    tasks[[i]] = list(id=i)
 }
-closeCluster(cl)
 
-print(temp)
+# Send the function to the slaves
+mpi.bcast.Robj2slave(mpiwrapper)
+
+# Call the function in all the slaves to get them ready to
+# undertake tasks
+mpi.bcast.cmd(mpiwrapper())
+
+# Create data structure to store the results
+result = as.list(rep(NA, length(tasks)))
+
+junk = 0 
+closed_slaves = 0 
+n_slaves = mpi.comm.size()-1 
+
+while (closed_slaves < n_slaves) { 
+		# Receive a message from a slave 
+		message = mpi.recv.Robj(mpi.any.source(), mpi.any.tag()) 
+
+		# ‘mpi.get.sourcetag’ finds the source and tag of a received message.
+		# 2 dim int vector. The first integer is the source and the second is
+		# the tag.
+
+		message_info = mpi.get.sourcetag() 
+
+		slave_id = message_info[1] 
+		tag = message_info[2] 
+
+		# Note the use of the tag for sent messages: 
+		#     1=ready_for_task, 2=done_task, 3=exiting 
+		# Note the use of the tag for received messages: 
+		#     1=task, 2=done_tasks 
+
+		if (tag == 1) { 
+				# slave is ready for a task. Give it the next task, or tell it tasks 
+				# are done if there are none. 
+				if (length(tasks) > 0) { 
+						# Send a task,  
+						mpi.send.Robj(tasks[[1]], slave_id, 1); 
+						# and then remove it from the task list
+						tasks[[1]] = NULL 
+				} 
+				else { 
+						mpi.send.Robj(junk, slave_id, 2) 
+				} 
+		} 
+		else if (tag == 2) { 
+				# The message contains results. Do something with the results. 
+				# Store them in the data structure
+				id = message$id
+				result[id] = message$result
+		} 
+		else if (tag == 3) { 
+				# A slave has closed down. 
+				closed_slaves=closed_slaves + 1 
+		} 
+} 
+
+
+print(result)
+
+mpi.close.Rslaves()
+mpi.quit(save="no")
+

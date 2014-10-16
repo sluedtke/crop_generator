2e392919ac0bc06a787ae3091221e4a879e33592
sluedtke <stefan-luedtke@gmx.de>
2014-10-16 20:48:18 +0200

    removed BOM as well

--- nuts_version.sql
+++ nuts_version.sql
@@ -1,4 +1,4 @@
-﻿-- This query returns the newest avalailable nuts version given any nuts code that was valid between 2003 and 2010
+-- This query returns the newest avalailable nuts version given any nuts code that was valid between 2003 and 2010
 -- Join the unique admin units from the time series ot our reference table
 -- If we find that multiple version fit, we take the most recent (max) one
 -- That will be the reference table for the time series.

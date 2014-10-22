d76e157a08275b9dfffee0e42be096476f1aca70
sluedtke <stefan-luedtke@gmx.de>
2014-10-22 13:35:52 +0200

    Added a unique identifier for each nuts and version.

--- nuts_version.sql
+++ nuts_version.sql
@@ -2,18 +2,11 @@
 -- Join the unique admin units from the time series ot our reference table
 -- If we find that multiple version fit, we take the most recent (max) one
 -- That will be the reference table for the time series.
-WITH 
 
 -- get the relevant nuts that occur in the time series
-unique_nuts AS (
-SELECT DISTINCT nuts_id
+SELECT DISTINCT ON (nuts_id)
+ nuts_id, 
+ oid_nuts
 FROM
- data.time_series)
-
--- and the version the correspond to
-SELECT id, nuts_code, MAX(nuts_version_year)
-from data.nuts_version_years
-INNER JOIN unique_nuts ON
-unique_nuts.nuts_id=data.nuts_version_years.nuts_code
-GROUP BY nuts_code, id;
+ data.time_series
 
